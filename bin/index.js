749235ec7dceba4d1f5bde1789095b5c78d75f6d
hashedhyphen <hashedhyphen@gmail.com>
2015-10-25 16:15:17 +0900

    Parse headers

--- bin/index.js
+++ bin/index.js
@@ -14,30 +14,22 @@ var _libPcapng_parserJs = require('./lib/pcapng_parser.js');
 
 var _libPcapng_parserJs2 = _interopRequireDefault(_libPcapng_parserJs);
 
-var readFile = function readFile(file_path) {
+var createParser = function createParser(file_path) {
   return new Promise(function (resolve, reject) {
     _fs2['default'].readFile(file_path, function (err, buf) {
       if (err) {
         reject(err);
       } else {
-        resolve(buf);
+        resolve(new _libPcapng_parserJs2['default'](buf));
       }
     });
   });
 };
 
-var createParser = function createParser(buf) {
-  return new Promise(function (resolve, reject) {
-    resolve(new _libPcapng_parserJs2['default'](buf));
-  });
-};
-
 exports['default'] = function (file_path) {
   return new Promise(function (resolve, reject) {
-    readFile(file_path).then(function (buf) {
-      resolve(new _libPcapng_parserJs2['default'](buf));
-    }).then(function (parser) {
-      resolve(42);
+    createParser(file_path).then(function (parser) {
+      resolve(parser.parse());
     })['catch'](function (err) {
       reject(err);
     });
